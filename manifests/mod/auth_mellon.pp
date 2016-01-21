76ce30e0ff16c0439faad9dc0b046680a0900165
pfgdrupalas <pfgdrupalas@users.noreply.github.com>
2016-01-21 05:47:17 -0600

    Update auth_mellon.pp
    added '-p' to apache::mod_dir

--- manifests/mod/auth_mellon.pp
+++ manifests/mod/auth_mellon.pp
@@ -18,7 +18,7 @@ class apache::mod::auth_mellon (
     path    => "${::apache::mod_dir}/auth_mellon.conf",
     mode    => $::apache::file_mode,
     content => template('apache/mod/auth_mellon.conf.erb'),
-    require => [ Exec["mkdir ${::apache::mod_dir}"], ],
+    require => [ Exec["mkdir -p ${::apache::mod_dir}"], ],
     before  => File[$::apache::mod_dir],
     notify  => Class['Apache::Service'],
   }
