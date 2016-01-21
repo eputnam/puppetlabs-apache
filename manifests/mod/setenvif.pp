07f1f14912bb5638c47938f8eb22aed9cdd2f0a2
pfgdrupalas <pfgdrupalas@users.noreply.github.com>
2016-01-21 07:12:03 -0600

    Update setenvif.pp
    added '-p' to apache::mod_dir

--- manifests/mod/setenvif.pp
+++ manifests/mod/setenvif.pp
@@ -7,7 +7,7 @@ class apache::mod::setenvif {
     path    => "${::apache::mod_dir}/setenvif.conf",
     mode    => $::apache::file_mode,
     content => template('apache/mod/setenvif.conf.erb'),
-    require => Exec["mkdir ${::apache::mod_dir}"],
+    require => Exec["mkdir -p ${::apache::mod_dir}"],
     before  => File[$::apache::mod_dir],
     notify  => Class['apache::service'],
   }
