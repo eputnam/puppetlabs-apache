6879a537393016619d18dc02002e42934908e8a6
pfgdrupalas <pfgdrupalas@users.noreply.github.com>
2016-01-21 06:32:18 -0600

    Update mime_magic.pp
    added '-p' to apache::mod_dir. updated magic_file variable default value

--- manifests/mod/mime_magic.pp
+++ manifests/mod/mime_magic.pp
@@ -1,5 +1,5 @@
 class apache::mod::mime_magic (
-  $magic_file = undef,
+  $magic_file = "${::apache::$server_root}/conf/magic"
 ) {
   include ::apache
   $_magic_file = pick($magic_file, "${::apache::conf_dir}/magic")
@@ -10,7 +10,7 @@ class apache::mod::mime_magic (
     path    => "${::apache::mod_dir}/mime_magic.conf",
     mode    => $::apache::file_mode,
     content => template('apache/mod/mime_magic.conf.erb'),
-    require => Exec["mkdir ${::apache::mod_dir}"],
+    require => Exec["mkdir -p ${::apache::mod_dir}"],
     before  => File[$::apache::mod_dir],
     notify  => Class['apache::service'],
   }
